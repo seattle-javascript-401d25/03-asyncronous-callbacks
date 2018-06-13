36070eafb7f0605b40760d05e17ecde792197aed
Tracy Williams <selpilot@gmail.com>
2018-06-13 16:00:24 -0700

    seems to be working. basic version

--- README.md
+++ README.md
@@ -46,3 +46,4 @@ Write the file reader function recursively so that it will be able to support 0 
 
 ##  Documentation
 Add your Travis CI build badge to the top of your README.md. Describe the exported values of each module you have defined. Every function description should include it's arity (expected number of parameters), the expected data for each paramiter (data-type and limitations), and it's behavior (for both valid and invalued use). Feel free to write any additional information in your README.md.
+
--- __test__/reader.test.js
+++ __test__/reader.test.js
@@ -1,7 +1,54 @@
 'use strict';
 
+const fs = require('fs');
+const readThreeFiles = require('../lib/reader');
+
+const files = [];
+const exData = [];
+
 describe('reader.js tests for lab 03', () => {
-  test('always true', () => {
-    expect(true).toBeTruthy();
+  beforeAll(() => {
+    files.push(`${__dirname}/./mock_data/test1.txt`);
+    files.push(`${__dirname}/./mock_data/test2.txt`);
+    files.push(`${__dirname}/./mock_data/test3.txt`);
+    exData.push(fs.readFileSync(files[0]).toString());
+    exData.push(fs.readFileSync(files[1]).toString());
+    exData.push(fs.readFileSync(files[2]).toString());
+  });
+  test('readThreeFiles tests', () => {
+    readThreeFiles(files, (err, data) => {
+      if (err) {
+        console.log('r3f unexpected error', err);
+        return;
+      }
+      expect(data).toHaveLength(3);
+      expect(data[0].toString()).toEqual(exData[0]);
+      expect(data[1].toString()).toEqual(exData[1]);
+      expect(data[2].toString()).toEqual(exData[2]);
+    });
+  });
+  test('readThreeFiles with bad fd[0]', () => {
+    const badFiles = files.slice(); // create copy so async behavior doesnt mess up tests above.
+    badFiles[0] = 'bad filename';
+    readThreeFiles(badFiles, (err, data) => { /* eslint-disable-line */
+      expect(err).toHaveProperty('errno');
+      expect(err.code).toEqual('ENOENT');
+    });
+  });
+  test('readThreeFiles with bad fd[1]', () => {
+    const badFiles = files.slice(); // create copy so async behavior doesnt mess up tests above.
+    badFiles[1] = 'bad filename';
+    readThreeFiles(badFiles, (err, data) => { /* eslint-disable-line */
+      expect(err).toHaveProperty('errno');
+      expect(err.code).toEqual('ENOENT');
+    });
+  });
+  test('readThreeFiles with bad fd[2]', () => {
+    const badFiles = files.slice(); // create copy so async behavior doesnt mess up tests above.
+    badFiles[2] = 'bad filename';
+    readThreeFiles(badFiles, (err, data) => { /* eslint-disable-line */
+      expect(err).toHaveProperty('errno');
+      expect(err.code).toEqual('ENOENT');
+    });
   });
 });
