d675261d7e3202c9111c9696556640d342bc0eca
Lizabeth Petersen <lizpetersen928@gmail.com>
2018-06-13 18:07:58 -0700

    adds data.txt files

--- src/lib/reader.js
+++ src/lib/reader.js
@@ -1,8 +1,14 @@
 'use strict';
 
-// const fs = require('fs');
-// const logger = require('./logger');
+const fs = require('fs');
+const logger = require('./logger');
 
-// const fileReader = module.exports= {};
+const fileReader = module.exports= {};
 
-// fileReader.readFile = ()
+// this reads a single file path
+fileReader.readFile = ( './src/lib/data/data1.txt' ) => {
+    return fs.readFile('./src/lib/data/data1.txt'), (err, data) => {
+        if err throw err;
+        logger.log(logger.INFO, data.toString());
+    }
+}
