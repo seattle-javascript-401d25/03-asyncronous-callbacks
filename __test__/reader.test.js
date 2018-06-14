1aae72c225748b861147aaee79bf83b81b786de0
Noah Israel <noahmvf@gmail.com>
2018-06-14 09:56:23 -0700

    filepaths and functionality fixed

--- __test__/reader.test.js
+++ __test__/reader.test.js
@@ -6,9 +6,9 @@ const fileReader = require('../lib/reader.js');
 
 //write test to ensure the reader function rejects errors with invalid file paths
 
-const filePath1 = `${__dirname}/./data/filepath1.txt`;
-const filePath2 = `${__dirname}/./data/filepath2.txt`;
-const filePath3 = `${__dirname}/./data/filepath3.txt`;
+const filePath1 = `${__dirname}/../data/filepath1.txt`;
+const filePath2 = `${__dirname}/../data/filepath2.txt`;
+const filePath3 = `${__dirname}/../data/filepath3.txt`;
 //these refer to our three files in our data folder that we will use to test if our reader.js functionality works properly
 
 let mockData = []; //this array will be filled with data from each of our filepaths above after it is read. This array is used for comparison to the data we return from our tests
@@ -23,7 +23,7 @@ describe('testing fileReader module that reads three files', () => {
   });
 
   test('should show that the data we read from readThreeFiles equals the data in our mockData array', () => {
-    fileReader.readThreeFiles(filePath1, filePath2, filePath3, (err, data1, data2, data3) => {
+    fileReader.readThreeFiles([filePath1, filePath2, filePath3], (err, data1, data2, data3) => {
       expect(data1).toEqual(filePath[0]); //we expect that our data1 argument of the callback in our readThreeFiles function equals the first filepath in our mockData array
       expect(data2).toEqual(filePath[1]);
       expect(data3).toEqual(filePath[2]);
